692ea2a68ec1d61f2b57827372984dc7d6bb848b
JonMid <jamideros@hotmail.com>
2018-10-26 17:41:20 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PlatziSquare';
+    title:string = 'My first AGM project';
+    lat:number = 4.6560663;
+    lng:number = -74.0595918;
+    color: string;
+
+    lugares:any = [
+        {id: 1, name: 'Floreria la gardenia'},
+        {id: 2, name: 'Donas la pasadita'},
+        {id: 3, name: 'Veterinaria Huellitas Felices'},
+        {id: 4, name: 'Hotel la gracia'}
+    ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { HighlightDirective } from './directives/highlight.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ResaltarDirective,
+        HighlightDirective,
+        ContarClicksDirective
+    ],
+    imports: [
+        BrowserModule,
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyBFJQZ1JaabtXjyRoJ9r-YIOtQp9vLzS14'
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
